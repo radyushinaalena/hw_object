56afeaafe4e2158c2a7f93f68427a16737fe6d32
Alena <al.shel@bk.ru>
2023-01-14 01:11:45 +0300

    ДЗ-2

--- src/pro/sky/java/course1/hw_object/Author.java
+++ src/pro/sky/java/course1/hw_object/Author.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.hw_object;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -9,10 +11,22 @@ public class Author {
         this.surname = surname;
     }
 
-    public String getName() {
-        return name;
+
+    @Override
+    public String toString() {
+        return "автор " + name +" "+ surname;
     }
-    public String getSurname() {
-        return surname;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
--- src/pro/sky/java/course1/hw_object/HomeWork1.java
+++ src/pro/sky/java/course1/hw_object/HomeWork1.java
@@ -6,9 +6,10 @@ public class HomeWork1 {
         Author gogol = new Author("Николай", "Гоголь");
         Book stranger = new Book("Незнакомка", blok, 1906);
         Book viy = new Book("Вий", gogol, 1833);
-        System.out.println("\"" + stranger.getTitle() + "\"" + " " + stranger.getAuthor().getName() + " " + stranger.getAuthor().getSurname() + " " + stranger.getPublishingYear());
-        System.out.println("\"" + viy.getTitle() + "\"" + " " + viy.getAuthor().getName() + " " + viy.getAuthor().getSurname() + " " + viy.getPublishingYear());
-        viy.setPublishingYear(1900);
-        System.out.println("\"" + viy.getTitle() + "\"" + " " + viy.getAuthor().getName() + " " + viy.getAuthor().getSurname() + " " + viy.getPublishingYear());
+
+        System.out.println(viy);
+        System.out.println(stranger);
+        System.out.println(viy.equals(stranger));
+
     }
 }
